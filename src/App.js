658342c3f59681e7ea89be47b82e878a50a4d678
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-07-08 16:40:53 +0530

    Feat : delete, css and filter functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import MyTable from "./component/Table";
+import Dropdown from "./component/Dropdown";
+
+import { useState } from "react";
+import Modal from "./component/Modal";
 
 function App() {
-  return <div className='App'>hello</div>;
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className='App'>
+      <div className='headerWrapper'>
+        <div className='header'>Team Members</div>
+        <button
+          className='addMemberBtn'
+          onClick={() => setOpenModal((open) => !open)}
+        >
+          + Add Members
+        </button>
+      </div>
+      <div className='bottomBorder'></div>
+      <Dropdown />
+      <MyTable />
+      <Modal openModal={openModal} setOpenModal={setOpenModal} />
+    </div>
+    // document.getElementById("portal")
+  );
 }
 
 export default App;
